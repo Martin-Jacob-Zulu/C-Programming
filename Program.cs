2b980272e116a57b06926479fc66ec1f89da9fd7
Martin Jacob Zulu <jacob.zulu@cs.unza.zm>
2021-05-02 00:58:22 +0200

    Getting Methods and Keywords in Csharp

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace C__Programming
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            foreach (var i in Assembly.GetEntryAssembly().GetReferencedAssemblies())
+            {
+                var a = Assembly.Load(new AssemblyName(i.FullName));
+
+                int methodCount = 0;
+
+                foreach (var j in a.DefinedTypes)
+                {
+                    methodCount += j.GetMethods().Count();
+                }
+
+                Console.WriteLine(
+                    "{0:N0} types with {1:N0} methods in {2} assembly.",
+                    arg0: a.DefinedTypes.Count(),
+                    arg1: methodCount,
+                    arg2: i.Name
+                );
+            }
+
+        }
+    }
+}
