5776b1c68f8c297a3e1df9d2cdd3a330640ff862
Martin Jacob Zulu <jacob.zulu@cs.unza.zm>
2021-05-02 12:37:00 +0200

    .

---
+++ Chapter3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Chapter3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var key = "";
+            var trials = 6;
+            while (key != "secrete")
+            {
+                Console.Write("Please enter a validation key : ");
+                key = Console.ReadLine();
+
+                if (key != "secrete")
+                {
+                    Console.WriteLine("Authentication Failed please try again.");
+                }
+                trials--;
+                Console.WriteLine($"{trials} remaining");
+                if (trials < 1)
+                {
+                    Console.WriteLine("Failed to authenticate");
+                    return;
+                }
+            }
+            Console.WriteLine("Successfully Authenticated");
+        }
+    }
+}
